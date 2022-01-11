0df04af051f3de8c1236898cc4c271297ca30f66
Alan Czemerinski <alanczeme@gmail.com>
2022-01-10 20:04:04 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(streetNumber) {
+    return Math.abs(streetNumber - 42);
+}
+
+function distanceFromHqInFeet(streetNumber) {
+    return Math.abs(streetNumber - 42) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs(start - end) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = Math.abs(start - destination) * 264;
+    switch (true) {
+        case distance > 2500:
+            return "cannot travel that far";
+        case distance > 2000:
+            return 25;
+        case distance > 400:
+            return (distance - 400) * 0.02
+        case distance <= 400:
+            return 0;
+    }
+}
+
+
